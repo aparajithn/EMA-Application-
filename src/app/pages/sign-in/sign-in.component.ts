d2cb950beac8ca6f71f17f7427459b775b62a39f
David Lengel <david@Davids-MacBook-Pro.local>
2019-11-26 23:18:11 -0500

    Complete sign-in failure scenarios

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -24,25 +24,54 @@ export class SignInComponent implements OnInit {
     async signInButtonTapped(): Promise<string> {
         let result_str = "";
 
-        await firebase.login(
-            {
-                type: firebase.LoginType.PASSWORD,
-                passwordOptions: {
-                    email: this.evaluationId + "@ema.org",
-                    password: this.password
-                }
-            })
-            .then(result => {
-                result_str = "Sign-in succeeded";
-                console.log("success sign in");
-                //console.log("RESULT EMAIL: " + JSON.stringify(result.email));
-                // save information and route to main page
-            })
-            .catch(error => {
-                result_str = "Sign-in failed";
-                console.log(error)
-            });
-
+        if(this.password.length < 6) {
+            result_str = "Sign-in failed: invalid password"
+            dialogs.alert({
+                title: "Unable to sign in",
+                message: "Password should be at least 6 characters long.",
+                okButtonText: "OK"
+            }).then(() => {})
+        }
+        else {
+            await firebase.login(
+                {
+                    type: firebase.LoginType.PASSWORD,
+                    passwordOptions: {
+                        email: this.evaluationId + "@ema.org",
+                        password: this.password
+                    }
+                })
+                .then(result => {
+                    result_str = "Sign-in succeeded";
+                    console.log("success sign in");
+                    //console.log("RESULT EMAIL: " + JSON.stringify(result.email));
+                    // save information and route to main page
+                })
+                .catch(error => {
+                    // if evaluation ID not found
+                    if (error.includes('FirebaseAuthInvalidUserException')) {
+                        result_str = "Sign-in failed: invalid evaluation ID";
+                        console.error("USER NOT FOUND");
+                        dialogs.alert({
+                            title: "Unable to sign in",
+                            message: "Evaluation ID does not exist.",
+                            okButtonText: "OK"
+                        }).then(() => {
+                        })
+                    } else if (error.includes('FirebaseAuthInvalidCredentialsException')) {
+                        result_str = "Sign-in failed: incorrect password";
+                        console.error("INCORRECT PASSWORD");
+                        dialogs.alert({
+                            title: "Unable to sign in",
+                            message: "The password is invalid or the user does not have a password.",
+                            okButtonText: "OK"
+                        }).then(() => {
+                        })
+                    } else {
+                        result_str = "Sign-in failed";
+                    }
+                });
+        }
         return result_str;
     }
 
--- src/tests/sign-in.test.ts
+++ src/tests/sign-in.test.ts
@@ -18,16 +18,48 @@ describe("Test sign-in success", function() {
 });
 
 describe("Test sign-in failure from invalid evaluation id", function() {
-    it("should return sign-in failed", function (done) {
+    it("should return sign-in failed invalid evaluation id", function (done) {
         let signInComponent = new SignInComponent(null);
 
-        // invalid credentials
-        signInComponent.evaluationId = "incorrect";
-        signInComponent.password = "valid_but_incorrect";
+        // invalid evaluation ID
+        signInComponent.evaluationId = "0000";
+        signInComponent.password = "abc123";
 
         signInComponent.signInButtonTapped()
             .then((result) => {
-                expect(result).toEqual("Sign-in failed");
+                expect(result).toEqual("Sign-in failed: invalid evaluation ID");
+                done();
+            });
+    });
+});
+
+describe("Test sign-in failure from incorrect password", function() {
+    it("should return sign-in failed incorrect password", function (done) {
+        let signInComponent = new SignInComponent(null);
+
+        // incorrect password
+        signInComponent.evaluationId = "8000";
+        signInComponent.password = "123abc";
+
+        signInComponent.signInButtonTapped()
+            .then((result) => {
+                expect(result).toEqual("Sign-in failed: incorrect password");
+                done();
+            });
+    });
+});
+
+describe("Test sign-in failure from invalid password", function() {
+    it("should return sign-in failed invalid password", function (done) {
+        let signInComponent = new SignInComponent(null);
+
+        // invalid password
+        signInComponent.evaluationId = "8000";
+        signInComponent.password = "123"; /* <6 characters */
+
+        signInComponent.signInButtonTapped()
+            .then((result) => {
+                expect(result).toEqual("Sign-in failed: invalid password");
                 done();
             });
     });
