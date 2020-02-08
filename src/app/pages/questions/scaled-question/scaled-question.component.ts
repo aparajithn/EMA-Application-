ac91d66d6199caf61c2596c6473c599239271d10
aparajithn <nagarajan.aparajith@gmail.com>
2020-02-08 14:41:56 -0500

    Done working on views for scaled question

--- src/app/pages/questions/scaled-question/scaled-question.component.ts
+++ src/app/pages/questions/scaled-question/scaled-question.component.ts
@@ -1,30 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Slider } from "tns-core-modules/ui/slider";
-import { fromObject } from "tns-core-modules/data/observable";
-import { BindingOptions } from "tns-core-modules/ui/core/bindable";
-import {TextField} from "ui/text-field";
-import {Label} from "ui/label";
-const person = {
+import { registerElement } from 'nativescript-angular/element-registry';
+import { CardView } from '@nstudio/nativescript-cardview';
+registerElement('CardView', () => CardView);
+const question = {
     id: 182,
     type:"Scaled",
-    text:"How much have you been bothered by repeated, disturbing, and unwanted memories of the stressful experience?",
-    lower_bound:10,
+    text1:"How much have you been bothered by repeated, disturbing, and unwanted memories of the stressful experience?",
+    lower_bound:0,
     upper_bound:4,
     lb_desc: "Not at all",
     up_desc: "Extremely"
 }
-const source = fromObject({
-    TextSource: "This text was created by two way data binding"
-});
-const targetTextField = new Label();
-const TextFieldBindingOptions: BindingOptions = {
-    sourceProperty: "TextSource",
-    targetProperty: "Text",
-    twoWay: true
-};
-targetTextField.bind(TextFieldBindingOptions, source);
-
 
 
 @Component({
@@ -34,9 +22,11 @@ targetTextField.bind(TextFieldBindingOptions, source);
     templateUrl: "./scaled-question.component.html"
 })
 export class ScaledQuestionComponent implements OnInit  {
-    maxValue = person.lower_bound;
-
-
+    maxValue = question.upper_bound;
+    minValue = question.lower_bound;
+    text:string = question.text1;
+    lb_desc:string = question.lb_desc;
+    up_desc:string = question.up_desc;
 
 
 
